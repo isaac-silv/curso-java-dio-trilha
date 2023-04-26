dc18f2d2d8e562f4d9ab4c939c151f4b95a86c70
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-26 16:49:35 -0300

    Adicionando exemplos de operadores lógicos

--- tipos-de-operadores/src/TiposOperadores.java
+++ tipos-de-operadores/src/TiposOperadores.java
@@ -112,5 +112,32 @@ public class TiposOperadores {
 
         if (numero1 != numero2)
             System.out.print("Numero 1 é diferente de numero 2");
+
+        // Operadores Logicos
+
+        // Os operadores lógicos, representam o recurso que nos permite criar expressões
+        // lógicas maiores, a partir da junção de duas ou mais expressões.
+        // && Operador Lógico "E"
+        // || Operador Lógico "OU"
+
+        // Operadores.java
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        /*
+         * Aqui estamos utilizando o operador lógico E para fazer a união de duas
+         * expressões.
+         * É como se estivesse escrito:
+         * "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+         */
+
+        if (condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");
+        ;
+
+        // Se condicao1 OU condicao2 for verdadeira, executar código.
+        if (condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
     }
 }
