cb084b71fb1bc2e5292a20e6f308c9182e7b2d47
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-26 10:46:13 -0300

    Adicionando exemplos de variávei

--- tipos-de-variaveis/src/TiposVariaveis.java
+++ tipos-de-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Variáveis numericas
+
+        byte idade = 18; // valor máximo 127 - valor mínimo -128
+        short ano = 2023; // valor máximo 32.767 - valor mínimo -32.768
+        int cep = 58059777; // valor máximo 2.147.483.647 - valor mínimo -2.147.483.648
+        long cpf = 12345678900L; // valor máximo 9.223.372.036.854.775.807 - valor mínmo
+                                 // -9.223.372.036.854.775.808
+        // Os valores do tipo long precisam terminar com L
+
+        float altura = 1.75F; // Valor máximo 6 - 7 dígitos
+        // Os valores do tipo float precisam terminar com F
+        double salario = 2500.99; // valor máximo 15 dígitos
     }
 }
