8b7755594022cf52a1a53b5258ec542415c8e43f
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-26 15:49:08 -0300

    Adicionando exemplos de operadores aritimeticos

---
+++ tipos-de-operadores/src/TiposOperadores.java
@@ -0,0 +1,28 @@
+public class TiposOperadores {
+    public static void main(String[] args) throws Exception {
+        // Tipos de Operadores
+
+        // Operador de atribuição '='
+        // Usado para atruibuir um valor em uma vaiável
+
+        String nome = "Isaac"; // exemplo
+
+        // Operadores aritimeticos
+        // Adição '+'
+        // Subtração '-'
+        // Mutiplicação '*'
+        // Divisão '/'
+
+        // Exemplo
+        int soma = 10 + 10;
+        int subtracao = 20 - 10;
+        int mutiplicacao = 3 * 10;
+        double divisao = 40 / 3;
+
+        // O operador '+' quando utilizado em uma variável do tipo texto realizará a
+        // concatenação
+
+        // Exemplo:
+        String conca = 10 + "10"; // o resultado será '1010'
+    }
+}
