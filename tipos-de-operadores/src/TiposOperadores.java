2b7b60251a055bdd112b56502e27833a545a98d1
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-26 15:54:25 -0300

    Adicionando exemplos de operadores unários

--- tipos-de-operadores/src/TiposOperadores.java
+++ tipos-de-operadores/src/TiposOperadores.java
@@ -24,5 +24,40 @@ public class TiposOperadores {
 
         // Exemplo:
         String conca = 10 + "10"; // o resultado será '1010'
+
+        // Operadores unário
+
+        // (+) Operador unário de valor positivo – números são positivos sem esse
+        // operador explicitamente;
+        // (-) Operador unário de valor negativo – nega um número ou expressão
+        // aritmética;
+        // (++) Operador unário de incremento de valor – incrementa o valor em 1
+        // unidade;
+        // (--) Operador unário de decremento de valor – decrementa o valor em 1
+        // unidade;
+        // (!) Operador unário lógico de negação – nega o valor de uma expressão
+        // booleana.
+
+        // classe Operadores.java
+        int numero = 5;
+
+        // Imprimindo o numero negativo
+        System.out.println(-numero);
+
+        // incrementando numero em mais 1 numero, imprime 6
+        numero++;
+        System.out.println(numero);
+
+        // incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        // ordem de precedencia conta aqui
+        System.out.println(++numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
     }
 }
