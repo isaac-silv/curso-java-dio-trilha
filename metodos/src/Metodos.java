974c83fb46a3fbcd37d6222f9e54e3198cf986b8
Isaac Silva <isaacaquino.dv@gmail.com>
2023-04-26 20:11:44 -0300

    Adicionando exemplos de metodos

---
+++ metodos/src/Metodos.java
@@ -0,0 +1,21 @@
+public class Metodos {
+    public static void main(String[] args) throws Exception {
+        // De ser nomeado como verbo
+        // Seguir o padrão camelCase
+
+        // Exemplos
+        /*
+         * somar(int n1, int n2){}
+         * 
+         * abrirConexao(){}
+         * 
+         * concluirProcessamento() {}
+         * 
+         * findById(int id){} // não se assuste, você verá muito método em inglês em sua
+         * jornada
+         * 
+         * calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma
+         * única finalidade
+         */
+    }
+}
