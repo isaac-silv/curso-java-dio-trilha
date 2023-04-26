a1e48102db46bb79a11709720c8b99ff5646cefa
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-26 16:08:59 -0300

    Adicionando exemplo de operadores ternário

--- tipos-de-operadores/src/TiposOperadores.java
+++ tipos-de-operadores/src/TiposOperadores.java
@@ -59,5 +59,30 @@ public class TiposOperadores {
         boolean verdadeiro = true;
 
         System.out.println("Inverteu " + !verdadeiro);
+
+        // Operador ternário
+
+        // O operador ternário é representado pelos símbolos "?" e ":"
+
+        // Exemplo:
+
+        // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*
+         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         */
+
+        // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a == b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
     }
 }
