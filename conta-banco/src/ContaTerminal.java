beba178ea4663f67ad7089139be04a8087a0cc9b
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-27 10:52:31 -0300

    Adicionando desafio conta bancaria

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá, Digite seu nome:");
+        String nome = sc.next();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = sc.next();
+
+        System.out.println("Digite o numero da conta:");
+        int conta = sc.nextInt();
+
+        System.out.println("Digite o valor do depósito:");
+        Double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia
+                + ", conta " + conta + " e seu saldo R$" + saldo + " já está disponivel para saque.");
+
+    }
+}
