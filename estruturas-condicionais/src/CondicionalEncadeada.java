508ad0d9d42741cf638fcb28d31c35663baac326
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-27 15:03:54 -0300

    Adicionando exemplo de condicional encadeada

---
+++ estruturas-condicionais/src/CondicionalEncadeada.java
@@ -0,0 +1,17 @@
+public class CondicionalEncadeada {
+    public static void main(String[] args) {
+        int nota1 = 8;
+        int nota2 = 6;
+
+        int media = nota1 + nota2 / 2;
+
+        if (media >= 7)
+            System.out.println("Aprovado");
+
+        else if (media >= 5 && media < 7)
+            System.out.println("Recuperação");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
