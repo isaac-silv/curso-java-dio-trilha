489fbf37a830ea093b7c3ffaa8cb84980a243c11
isaac-silv <isaacaquino.dv@gmail.com>
2023-04-27 16:21:10 -0300

    Adicionando exemplo de loop for

---
+++ estruturas-de-repetição/src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) throws Exception {
+        for (int contagem = 1; contagem <= 10; contagem++) {
+            System.out.println("Contagem do loop " + contagem);
+        }
+        System.out.println("LOOP encerrado");
+    }
+}
